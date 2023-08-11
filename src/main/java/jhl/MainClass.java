87ff7684389d5d33a146a94afdb1593a80816607
junheiLee <jun2ee22@naver.com>
2023-08-11 10:55:47 +0900

    main Solution 변수명 변경

--- src/main/java/jhl/MainClass.java
+++ src/main/java/jhl/MainClass.java
@@ -1,11 +1,11 @@
 package jhl;
 
-import jhl.programmers._12939.Solution12939;
+import jhl.programmers._131128.Solution131128;
 
 public class MainClass {
 
     public static void main(String[] args) {
-        Solution _12939 = new Solution12939();
-        _12939.execute();
+        Solution solution = new Solution131128();
+        solution.execute();
     }
 }
