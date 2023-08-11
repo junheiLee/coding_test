18f0738e8c1888e14bf758fe0cd75ea89db6332a
junheiLee <jun2ee22@naver.com>
2023-08-11 16:31:28 +0900

    baekjoon: 문자열 출력하기

---
+++ src/main/java/jhl/baekjoon/string/_11718.java
@@ -0,0 +1,16 @@
+package jhl.baekjoon.string;
+
+import java.util.Scanner;
+
+public class _11718  {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		do {
+			System.out.println(sc.nextLine());
+			
+		} while(sc.hasNext());
+		
+	}
+}
