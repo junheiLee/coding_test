47c5bace13d7932fb6244c599decbc78de88c5b5
junheiLee <jun2ee22@naver.com>
2023-08-12 19:43:36 +0900

    baekjoon: 그룹 단어 체커2

--- src/main/java/jhl/baekjoon/string/_1316.java
+++ src/main/java/jhl/baekjoon/string/_1316.java
@@ -8,6 +8,38 @@ public class _1316 {
 		Scanner sc = new Scanner(System.in);
 
 		int n = Integer.parseInt(sc.nextLine());
+		int result = n;
+
+		for (int i = 0; i < n; i++) {
+			String word = sc.nextLine();
+
+			boolean[] visited = new boolean[26]; // 알파벳 개수
+			visited[word.charAt(0) - 'a'] = true; // 첫번째 글자 방문
+
+			for (int j = 1; j < word.length(); j++) {
+
+				if (word.charAt(j) == word.charAt(j - 1)) { // 연속된 알파벳
+					continue;
+				}
+				
+				if (visited[word.charAt(j) - 'a'] == true) { // 연속되지 않았는데, 방문한 적 있는 알파벳
+					result--;
+					break;
+				}
+				
+				visited[word.charAt(j) - 'a'] = true; // 연속되지 않고, 방문한 적 없으면 방문으로 변경
+				
+			}
+		}
+		
+		System.out.println(result);
+
+	}
+
+	private static void checker01() {
+		Scanner sc = new Scanner(System.in);
+
+		int n = Integer.parseInt(sc.nextLine());
 		String[] words = new String[n];
 
 		for (int i = 0; i < n; i++) {
@@ -36,7 +68,5 @@ public class _1316 {
 			}
 		}
 		System.out.println(result);
-
 	}
-
 }
